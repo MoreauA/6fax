0bdb8b1e0e4eb694523c3cb032182ea4227bb798
Caybe <cabezasmathias@gmail.com>
2019-02-04 19:23:16 +0100

    welcome  view

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import pygame
+from pygame.locals import *
+import pygame.gfxdraw
 
 pygame.init()
 
-window = pygame.display.set_mode((640,480))
+# welcome view
+window = pygame.display.set_mode((1200,640))
+window.fill((255,255,255))
+runWelcome = 1
 
-run = 1
+Rectplace = pygame.gfxdraw.box(window, (100, 120, 100, 100), [0, 0, 255])
+pygame.display.update()
 
-# begin view
-while run:
-    continue
+while runWelcome:
+    #loop to quit
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            runWelcome = 0
+
+    pos = pygame.mouse.get_pos()
+    if Rectplace.collidepoint(pos):
+        print("success")
