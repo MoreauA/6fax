a6cad6a84707f3cac14a4520bae1a6c3f12c65ff
William-Peel <39760445+William-Peel@users.noreply.github.com>
2019-02-05 21:27:45 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Model/Mob.py
    #	Model/Wave.py

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ pygame.init()
 NBLEVEL = 10
 maps = []
 
-
 # =========================================================================================================================================
-# Welcome view :
+# Window :
 window = pygame.display.set_mode((1024, 768))
 pygame.display.set_caption("Tacos Mania")
 window.fill((255, 255, 255))
@@ -56,7 +55,7 @@ def chooseMaps():
                        mapGame(window, int(b.text))
                        runChooseMap = False
 
-#=========================================================================================================================================
+# =========================================================================================================================================
 # Welcome View
 widthButton = 250
 posXButton = (pygame.display.get_surface().get_width() / 2) - ((1/2) * widthButton)
@@ -65,40 +64,40 @@ posYButton = (pygame.display.get_surface().get_height() / 3)
 # Buttons on the top
 start = button((0, 200, 0), posXButton, posYButton - 200, widthButton, 80, '', 'Tacos Mania') # To delete one day
 
-#Buttons on the middle
+# Buttons on the middle
 arena = []
 widthButton = 150
-for i in range(0,10):
+for i in range(0, 10):
     arenaName = "Arène " + str(i+1)
     if i < 5:
-        arena.append(button((0, 0, 200), (posXButton -350) + i*200, posYButton, widthButton, 80,'', arenaName))
+        arena.append(button((0, 0, 200), (posXButton - 350) + i*200, posYButton, widthButton, 80, '', arenaName))
     else:
-        arena.append(button((0, 0, 200), (posXButton -350) + (i-5)*200, posYButton + 200, widthButton, 80,'', arenaName))
+        arena.append(button((0, 0, 200), (posXButton - 350) + (i-5)*200, posYButton + 200, widthButton, 80, '', arenaName))
 
-#Buttons on the bottom
+# Buttons on the bottom
 widthButton = 250
-boutique = button((0, 0, 200), posXButton -350 , posYButton + 400, widthButton, 80,'', 'Boutique')
-score = button((0, 0, 200), posXButton, posYButton + 400, widthButton, 80,'', 'Score')
-quit = button((200, 0, 0), posXButton +350, posYButton + 400, widthButton, 80,'', 'Quitter')
+boutique = button((0, 0, 200), posXButton - 350, posYButton + 400, widthButton, 80, '', 'Boutique')
+score = button((0, 0, 200), posXButton, posYButton + 400, widthButton, 80, '', 'Score')
+quit = button((200, 0, 0), posXButton + 350, posYButton + 400, widthButton, 80, '', 'Quitter')
 
 def redrawWindow():
     window.fill((255, 255, 255))
     start.draw(window)
 
-    for val in range(0,10):
+    for val in range(0, 10):
         arena[val].draw(window)
 
     boutique.draw(window)
     score.draw(window)
     quit.draw(window)
-    #window.blit(imgAubergine, (10, 20))
+    # window.blit(imgAubergine, (10, 20))
 
 while runWelcome:
     redrawWindow()
     pygame.display.update()
 
     for event in pygame.event.get():
-        if event.type == QUIT: #If you click on the window's cross
+        if event.type == QUIT: # If you click on the window's cross
             runWelcome = False
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             pos = pygame.mouse.get_pos()
