cb110693a6573abf4c502d0f4fe92b32772a52fe
Caybe <cabezasmathias@gmail.com>
2019-02-07 16:23:19 +0100

    bouton scores

--- View/drawMap.py
+++ View/drawMap.py
@@ -2,7 +2,7 @@ import pygame
 import math
 from Model.Map import *
 
-LIFE = pygame.image.load('View/Data/Map/Coeur.png')
+LIFE = pygame.image.load('View/Data/Map/coeur.png')
 
 SALADE = pygame.image.load('View/Data/Monster/Salade.png')
 AUBERGINE = [pygame.image.load('View/Data/Monster/Aubergine/1.png'), pygame.image.load('View/Data/Monster/Aubergine/2.png'), pygame.image.load('View/Data/Monster/Aubergine/4.png')]
