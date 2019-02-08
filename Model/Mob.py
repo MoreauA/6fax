49cb69d95bfff9e33020e0ae0304bb10e4b0c9db
cdlk <popoto38697@gmail.com>
2019-02-08 04:45:28 +0100

    Tomate animer !

--- Model/Mob.py
+++ Model/Mob.py
@@ -127,7 +127,21 @@ class Tomate(Monster):
     SPEED = 0.4
 
     def __init__(self, wall):
-        Monster.__init__(self, self.VALUE, self.MAXLIFE, [25, 25], 0, self.SPEED, wall)
+        Monster.__init__(self, self.VALUE, self.MAXLIFE, [50, 75], 0, self.SPEED, wall)
+        self.state = 0
+        self.animation = 0
+        if wall==1:
+            self.right = True
+            self.left = False
+        elif wall == 2:
+            self.right = True
+            self.left = False
+        elif wall == 3:
+            self.right = False
+            self.left = True
+        else:
+            self.right = False
+            self.left = True
 
     def move(self):
         # Le déplacement sur le sol ou le plafond :
@@ -136,9 +150,13 @@ class Tomate(Monster):
             if self.pos[0] + self.size[0] > MAXPOSXWALL:
                 self.pos[0] = MAXPOSXWALL - self.size[0]
                 self.speed[0] = -self.speed[0]
+                self.right = not self.right
+                self.left = not self.left
             elif self.pos[0] < MINPOSXWALL:
                 self.pos[0] = MINPOSXWALL
                 self.speed[0] = -self.speed[0]
+                self.right = not self.right
+                self.left = not self.left
 
         # Le déplacement sur les murs de gauche et de droite
         elif self.wall == 2 or self.wall == 4:
@@ -146,9 +164,13 @@ class Tomate(Monster):
             if self.pos[1] + self.size[1] > MAXPOSYWALL:
                 self.pos[1] = MAXPOSYWALL - self.size[1]
                 self.speed[1] = -self.speed[1]
+                self.right = not self.right
+                self.left = not self.left
             elif self.pos[1] < MINPOSYWALL:
                 self.pos[1] = MINPOSYWALL
                 self.speed[1] = -self.speed[1]
+                self.right = not self.right
+                self.left = not self.left
 
     def attack(self):
         pass
